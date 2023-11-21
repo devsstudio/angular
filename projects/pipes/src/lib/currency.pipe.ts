6dfbdf03f78d36fd394626d4dd316bc66f831342
jean.perea <jean.perea@rappi.com>
2023-11-21 00:45:28 -0500

    Se corrige pipe de moneda

--- projects/pipes/src/lib/currency.pipe.ts
+++ projects/pipes/src/lib/currency.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'currencyPipe',
 })
 export class CurrencyPipe implements PipeTransform {
-  transform(value: any, args?: any): any {
-    return 'S/' + Number(value).toFixed(2);
+  transform(value: any, currency: 'USD' | 'PEN'): any {
+    return (currency === 'USD' ? '$' : 'S/') + Number(value).toFixed(2);
   }
 }
