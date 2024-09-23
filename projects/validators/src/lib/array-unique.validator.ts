87d8d9aa250f2c1aaa3376ed3bc5e8c73aa4e775
jean.perea <jean.perea@rappi.com>
2024-09-22 23:43:22 -0500

    improving array unique validator

--- projects/validators/src/lib/array-unique.validator.ts
+++ projects/validators/src/lib/array-unique.validator.ts
@@ -11,46 +11,50 @@ export enum ArrayUniqueProcessType {
     SPACES = "SPACES"
 }
 
-function _checkDuplicates(inputs: ArrayUniqueItem[], fields: string[], processType: ArrayUniqueProcessType) {
+export class ArrayUnique {
+    static checkDuplicates(inputs: ArrayUniqueItem[], fields: string[], processType: ArrayUniqueProcessType): boolean {
 
-    var items = inputs.reduce<string[]>((acc, cur) => {
-        // Verificar si cur es un objeto vacío
-        if (Object.keys(cur).length === 0) {
-            return acc; // Ignorar objetos vacíos
-        }
-
-        var parts = [];
-        for (let field of fields) {
-            var value = cur[field].toLowerCase();
-            switch (processType) {
-                case ArrayUniqueProcessType.ALIAS:
-                    parts.push(generateAlias(value));
-                    break;
-                case ArrayUniqueProcessType.SPACES:
-                    parts.push(value.replace(/\s/g, ''));
-                    break;
-                default:
-                    parts.push(value);
-                    break;
+        var items = inputs.reduce<string[]>((acc, cur) => {
+            // Verificar si cur es un objeto vacío
+            if (Object.keys(cur).length === 0) {
+                return acc; // Ignorar objetos vacíos
             }
-        }
-        acc.push(parts.join('|'));
-        return acc;
-    }, []);
 
-    var uniques = items.filter((value, index, self) => {
-        return self.indexOf(value) === index;
-    });
+            var parts = [];
+            for (let field of fields) {
+                var value = cur[field].toLowerCase();
+                switch (processType) {
+                    case ArrayUniqueProcessType.ALIAS:
+                        parts.push(generateAlias(value));
+                        break;
+                    case ArrayUniqueProcessType.SPACES:
+                        parts.push(value.replace(/\s/g, ''));
+                        break;
+                    default:
+                        parts.push(value);
+                        break;
+                }
+            }
+            acc.push(parts.join('|'));
+            return acc;
+        }, []);
 
-    return items.length === uniques.length;
+        var uniques = items.filter((value, index, self) => {
+            return self.indexOf(value) === index;
+        });
+
+        return items.length === uniques.length;
+    }
 }
+
+
 
 export function checkArrayUnique(fields: string[], processType: ArrayUniqueProcessType = ArrayUniqueProcessType.CASEINSENSITIVE): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
 
         const items = control.value as ArrayUniqueItem[];
 
-        if (!_checkDuplicates(items, fields, processType)) {
+        if (!ArrayUnique.checkDuplicates(items, fields, processType)) {
             return { unique: { value: control.value } };
         }
 
